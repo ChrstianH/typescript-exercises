6a36a68ab15e945bdb622b3876e54720a6a6fb43
ChrstianH <henschel_christian@web.de>
2024-08-23 08:54:39 +0200

    24 Interfaces Bonus unfinished; 26 Project

--- src/24_interfaces/OOP-Interfaces-TS-Level-3_1/app.ts
+++ src/24_interfaces/OOP-Interfaces-TS-Level-3_1/app.ts
@@ -4,12 +4,42 @@ const selectElement = document.getElementById(
   "iceCream-select"
 ) as HTMLSelectElement;
 
+const displayAllBtn = document.getElementById(
+  "displayAll"
+) as HTMLButtonElement;
+const addBtn = document.getElementById("addBtn") as HTMLButtonElement;
+const delBtn = document.getElementById("delBtn") as HTMLButtonElement;
+const availableBtn = document.getElementById(
+  "availableBtn"
+) as HTMLButtonElement;
+const displayPriceBtn = document.getElementById(
+  "displayPrice"
+) as HTMLButtonElement;
+const outputElement = document.getElementById("output") as HTMLDivElement;
+
 const iceCreamParlor: IceCreamParlor = new IceCreamParlor();
+
 const flavorArray = iceCreamParlor.listFlavors();
-flavorArray.forEach((flavor: string) => {
+flavorArray.forEach((flavor: string, index: number) => {
   const optionEl = document.createElement("option") as HTMLOptionElement;
   optionEl.value = flavor;
   optionEl.textContent = flavor;
+  optionEl.setAttribute("selected", (index === 0).toString());
   selectElement.appendChild(optionEl);
-  //  selectElement.setAttribute("size", flavorArray.length.toString());
+});
+
+displayAllBtn.addEventListener("click", (event: MouseEvent) => {
+  event.preventDefault();
+});
+addBtn.addEventListener("click", (event: Event) => {
+  event.preventDefault();
+});
+delBtn.addEventListener("click", (event: Event) => {
+  event.preventDefault();
+});
+availableBtn.addEventListener("click", (event: Event) => {
+  event.preventDefault();
+});
+displayPriceBtn.addEventListener("click", (event: Event) => {
+  event.preventDefault();
 });
